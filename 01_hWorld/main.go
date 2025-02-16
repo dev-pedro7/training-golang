53fd50a479109dcfbf4262c5dfc226920af57bad
dev-pedro7 <pedr2046@gmail.com>
2025-02-16 12:35:25 -0300

    01

---
+++ 01_hWorld/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var greet string = "Welcome"
+	guyName := "Nazario"
+
+	fmt.Printf("Hello, World %s\n", guyName)
+	fmt.Println(greet)
+	// Comentarios
+
+}
---
+++ 02_nums/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var decNumber int
+	fmt.Print("Decimal para Binario: ")
+	fmt.Scanf("%d", &decNumber)
+	fmt.Printf("%b", decNumber)
+}
